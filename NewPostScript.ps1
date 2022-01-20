c8c508cede6c46642f5781462c0c379bcd5425d2
Mitesh Shah <miteshrocker@gmail.com>
2022-01-20 18:42:21 +0530

    Adding a script to bootstrap a new post

---
+++ NewPostScript.ps1
@@ -0,0 +1,29 @@
+# New Post Script to bootstrap a new Markdown post
+
+function SanitizeFileTitle {
+    param ($UnsanitizedTitle)
+
+    $SanitizedTitle = $UnsanitizedTitle.Replace(" ", "-").ToLower()
+    return $SanitizedTitle
+}
+
+$FileTitle = Read-Host -Prompt "Enter the name of the file"
+
+$Title = Read-Host -Prompt "Enter the title of the post"
+
+# Create the file name
+
+$DateToday = Get-Date -Format yyyy-mm-dd
+$FileName = $DateToday  + "-" + $FileTitle +  ".md"
+
+$FileContent = "---
+toc: false
+layout: post
+description: 
+categories: 
+title: $Title
+comments: true
+---
+"
+
+New-Item -Path "_posts/" -Name $FileName -Type File -Value $FileContent
